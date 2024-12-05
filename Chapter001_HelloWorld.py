66ed87f81fb93efbf5f9c64f7d962d56129b3f0f
vikas9872 <shanabhogvikas648.com>
2024-12-05 18:23:21 +0530

    its fine

--- Chapter001_HelloWorld.py
+++
@@ -1,9 +0,0 @@
-# Print statement- Single and Double inverted commas can be used
-print('Hello World!! in Single inverted commas')
-print("Hello World!! in Doube inverted commas")
-
-# 4 patterns of code:
-# 1. Sequential
-# 2. Conditional
-# 3. Iterations
-# 4. Fuctions
